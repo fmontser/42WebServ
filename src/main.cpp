436b2d09064ab41af66dc1aa2846193c129506d9
Boris Randebrock <brandebr@student.42barcelona.com>
2025-01-08 12:41:59 +0100

    happy new year

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
 	(void)argc;
 	(void)argv;
+#ifdef __APPLE__
+	system("open new-years.gif");
+#elif __linux__
+	system("xdg-open 200w.gif");
+	sleep(3);
+	system("xdg-open new-years.gif");
+#elif _WIN32
+	system("start new-years.gif");
+#endif
 	std::cout << "Hello Wally & happy new Year" << std::endl;
 	return 0;
 }
