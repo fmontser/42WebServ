42012f5ac4bf5d284496e5d8d00132d1e0c95a7a
Boris Randebrock <brandebr@student.42barcelona.com>
2025-01-09 11:10:24 +0100

    happy new year 3.0

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,17 @@ int main(int argc, char** argv) {
 	(void)argc;
 	(void)argv;
 #ifdef __APPLE__
-//	system("open new-years.gif");
-	system("osascript -e 'tell application \"Preview\" to open POSIX file \"new-years.gif\"' -e 'set the bounds of window 1 to {100, 100, 500, 500}'");
-	system("osascript -e 'tell application \"Preview\" to open POSIX file \"200w.gif\"' -e 'set the bounds of window 1 to {600, 100, 1000, 500}'");
+	system("open new-years.gif");
+	sleep(3);
+	system("open 201w.gif");
+	sleep(3);
+	system("open 200w.gif");
 #elif __linux__
 	system("xdg-open 201w.gif &");
 	sleep(3);
 	system("xdg-open 200w.gif &");
 	sleep(3);
-//	system("xdotool search --sync --name 200w.gif windowmove 100 100");
 	system("xdg-open new-years.gif &");
-//	system("xdotool search --sync --name new-years.gif windowmove 600 100");
 #elif _WIN32
 	system("start new-years.gif");
 #endif
