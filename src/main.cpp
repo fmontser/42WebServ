479262a6ca3f3b80a16c1782acc8527eca4848a7
Lineath <fran.montserrat.developer@gmail.com>
2025-01-23 15:39:28 +0100

    Class diagram work

--- src/main.cpp
+++ src/main.cpp
@@ -24,23 +24,4 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-<<Processor>>
-CgiProcessor
---
-- response : HttpResponse
---
-+ CgiProcessor() 
-+ ~CgiProcessor()
-- CgiProcessor(const CgiProcessor& src)
-- operator=(const CgiProcessor& src) : const CgiProcessor&
-
-+ recieveHttpResponse(HttpResponse& Response) : void
-- processHttpResponse() : void
-- setEnviorment() : void
-- executeCGI();
---
-Input: FileManager -> HttpResponse
-Output: HttpResponse -> FileManager
-
-Manage cgi response modification
 
